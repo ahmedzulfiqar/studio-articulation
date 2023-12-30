0236ec3357dc8d48e8f0118f85d5afbbecfa5de7
ahmedzulfiqar <ahmdzulfiqar01@example.com>
2023-12-30 17:23:17 +0500

    d

--- src/components/blocks/home-blocks/Abouthomeetext.jsx
+++ src/components/blocks/home-blocks/Abouthomeetext.jsx
@@ -5,17 +5,6 @@ function Abouthomeetext() {
   return (
     <motion.div
       className="col-lg-6  justify-content-center  ps-lg-5  col-12 p-0 d-flex align-items-center text-lg-start text-center"
-      viewport={{ once: true }}
-      initial="hidden"
-      whileInView="visible"
-      variants={{
-        visible: { opacity: 1, x: 0 },
-        hidden: {
-          opacity: 0,
-          x: 100,
-        },
-      }}
-      transition={{ duration: 1, delay: 0.3 }}
     >
       <div className="p-0 w-100">
         <div className="display-5 ms-lg-1 mb-lg-4 mb-3 fw-bolder d-lg-block d-none">
